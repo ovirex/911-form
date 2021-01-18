1a6ea1620fae97696d3d428780b751f049376a72
Ovidio Pérez <oapm10@gmail.com>
2021-01-17 21:11:37 -0400

    Move index.html file outside the public folder

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 
+const nodemailer = require("nodemailer");
+
 const port = 3000;
-app.use(express.static("index.html"));
+
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>");
+    res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/contact", (req, res) => {});
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
